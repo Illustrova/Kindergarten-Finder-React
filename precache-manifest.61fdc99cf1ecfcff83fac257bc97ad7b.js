b146db6995cb03076652ed319e91e06376cf0a7c
Irina Illustrova <illustrova@gmail.com>
2019-05-20 18:03:05 +0300

    Updates

---
+++ precache-manifest.61fdc99cf1ecfcff83fac257bc97ad7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0239843ec45f01c7e81f4fe9b1e8082a",
+    "url": "/Kindergarten-Finder-React/index.html"
+  },
+  {
+    "revision": "87041d00afb3124cd5af",
+    "url": "/Kindergarten-Finder-React/static/css/main.2063021d.chunk.css"
+  },
+  {
+    "revision": "f6229890e6d486f9cb14",
+    "url": "/Kindergarten-Finder-React/static/js/2.aa24702d.chunk.js"
+  },
+  {
+    "revision": "87041d00afb3124cd5af",
+    "url": "/Kindergarten-Finder-React/static/js/main.24896da9.chunk.js"
+  },
+  {
+    "revision": "f56b7c31131674e53fc9",
+    "url": "/Kindergarten-Finder-React/static/js/runtime~main.355b80da.js"
+  }
+]);
